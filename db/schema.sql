c45675887a9e0b41cf8b649abfe49c0d32f9f7d0
por-chang <115598817+por-chang@users.noreply.github.com>
2023-04-05 19:39:14 -0500

    Add files via upload

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+-- lacks a "DROP DATABASE IF EXISTS tBuilding_db;" as this will be a product to be run on servers potentially containing sensitive databases"
+
+-- check for database conflict before running
+DROP DATABASE IF EXISTS tLeash_db;
+-- remove above code before project completion
+
+
+
+CREATE DATABASE tLesh_db;
+
+USE tLeash_db
