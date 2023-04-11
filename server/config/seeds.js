9f80ded4a2d4c1d39475bd501914ef6ef39f01c3
eSTee3 <31021627+eSTee3@users.noreply.github.com>
2023-04-11 09:31:01 -0500

    Update seeds.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,16 +1,52 @@
-const seed = require('seed');
-const db = require('./config/connection');
+// Used the following link to create this:  https://plainenglish.io/blog/seeding-mongodb-database-from-node-the-simplest-way
 
-const startServer = async () => {
-  await seed(); // Seed the database
+const mongoose = require('mongoose');
+const { User, Bark } = require('../models');
 
-  // Start the server
-  await db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
+mongoose.connect('mongodb://localhost:27017/test', {
+  useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MONGO CONNECTION OPEN!!!');
+  })
+  .catch((err) => {
+    console.log(err);
   });
-};
 
-startServer();
+  const seedUsers = [
+    {
+      userName: 'TestUser01',
+      email: 'testuser01@gmail.com',
+      password: '123456',
+      bark: [{
+        description: "This is my very first bark!!!",
+        likes: 8
+      }]
+    },
+    {
+      userName: 'TestUser02',
+      email: 'testuser02@gmail.com',
+      password: '123456',
+      bark: [{
+        description: "I adore this site",
+        likes: 87
+      }]
+    },
+    {
+      userName: 'TestUser02',
+      email: 'testuser02@gmail.com',
+      password: '123456',
+      bark: [{
+        description: "I can't believe it",
+        likes: 1
+      }]
+    },
+  ];
+
+  const seedDB = async () => {
+    await User.deleteMany({});
+    await User.insertMany(seedUsers);
+  };
+
+  seedDB().then(() => {
+    mongoose.connection.close();
+  });
